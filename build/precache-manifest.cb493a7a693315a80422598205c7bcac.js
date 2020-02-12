f319463be23160a3278fef604256f8dc206f6172
aalvarez <alvarez.vargasgmail.com>
2020-02-11 21:06:36 -0300

    Se crea build del proyecto y se sube a netlify.com

---
+++ build/precache-manifest.cb493a7a693315a80422598205c7bcac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026025ff4e863c20f1e5edeeb59a7c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351dc33fda1b41292ff6",
+    "url": "/static/css/main.1c201025.chunk.css"
+  },
+  {
+    "revision": "7c91f6f7163a45d3991e",
+    "url": "/static/js/2.e99c3866.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e99c3866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351dc33fda1b41292ff6",
+    "url": "/static/js/main.1b7e0101.chunk.js"
+  },
+  {
+    "revision": "7deb0b962a4b429f30c9",
+    "url": "/static/js/runtime-main.a6b39ca9.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
